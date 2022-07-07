fad3ff68ef6cc96245b0c67727b4bf2f91f8222a
Wissam-Ben <benadjaoud.wissam@gmail.com>
2022-07-07 19:28:41 +0200

    final

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,7 +5,7 @@ use std::io::{Write, Read, stdin, stdout};
 use serde_json::from_str;
 use std::str::from_utf8;
 use rand::Rng;
-use shared::{ChallengeAnswer, ChallengeResult, Message, PublicPlayer, Subscribe, SubscribeResult, Welcome};
+use shared::{ChallengeAnswer, ChallengeResult, Message, PublicPlayer, Subscribe, SubscribeResult};
 use shared::Challenge::{MD5HashCash, MonstrousMaze};
 
 
@@ -25,59 +25,91 @@ fn exchange_with_server(mut stream: TcpStream) {
 
     shared::send(&mut stream, Message::Hello);
     let response = shared::receive(&mut stream);
+    let res = from_utf8(&response);
+    match res {
+        Ok(resp) => {
+            println!("{}", resp);
+        }
+        Err(err) => {
+            println!("{}", err);
+        }
+    }
 
-    println!("{}", from_utf8(&response).unwrap());
-
-    shared::send(&mut stream, Message::Subscribe(Subscribe { name: "wiw".to_string() }));
+    shared::send(&mut stream, Message::Subscribe(Subscribe { name: "wissssam".to_string() }));
     let response = shared::receive(&mut stream);
-    println!("{}", from_utf8(&response).unwrap());
+    let res = from_utf8(&response);
+    match res {
+        Ok(resp) => {
+            println!("{}", resp);
+        }
+        Err(err) => {
+            println!("{}", err);
+        }
+    }
 
     loop {
         let response = shared::receive(&mut stream);
-        let response= from_utf8(&response).unwrap();
-        println!("{}", response);
-        let response = serde_json::from_str(response);
-        match response {
-            Ok(res) => {
-                match res {
-                    Message::EndOfGame(..) => {
-                        break;
-                    }
-                    Message::Challenge(response) => {
-                        match response {
-                            MD5HashCash(md5_hash_cash_input) => {
-                                shared::send(&mut stream, Message::ChallengeResult(
-                                    ChallengeResult {
-                                        answer: ChallengeAnswer::MD5HashCash {
-                                            0: hash_cash_challenge::md5hashage(md5_hash_cash_input),
-                                        },
-                                        next_target: "".to_string()
-                                    }
-                                ));
-                                let response = shared::receive(& mut stream);
-                                println!("{}", from_utf8(&response).unwrap());
-                            },
-                            MonstrousMaze(monstrous_maze_input) => {
-                                break;
-                            }
-                        }
-                    }
-                    Message::SubscribeResult(res) => {
+        let res = from_utf8(&response);
+        match res {
+            Ok(resp) => {
+                let response = serde_json::from_str(resp);
+                match response {
+                    Ok(res) => {
                         match res {
-                            SubscribeResult::Ok => {}
-                            SubscribeResult::Err(..) => {
+                            Message::EndOfGame(..) => {
                                 break;
                             }
+                            Message::Challenge(response) => {
+                                match response {
+                                    MD5HashCash(md5_hash_cash_input) => {
+                                        shared::send(&mut stream, Message::ChallengeResult(
+                                            ChallengeResult {
+                                                answer: ChallengeAnswer::MD5HashCash {
+                                                    0: hash_cash_challenge::md5hashage(md5_hash_cash_input),
+                                                },
+                                                next_target: "{\"Challenge\":{\"MD5HashCash\":{\"complexity\":5,\"message\":\"Hello\"}}}".to_string()
+                                            }
+                                        ));
+                                        let response = shared::receive(& mut stream);
+                                        let res = from_utf8(&response);
+                                        match res {
+                                            Ok(resp) => {
+                                                println!("{}", resp);
+                                            }
+                                            Err(err) => {
+                                                println!("{}", err);
+                                            }
+                                        }
+
+                                    },
+                                    MonstrousMaze(monstrous_maze_input) => {
+                                        break;
+                                    }
+                                }
+                            }
+                            Message::SubscribeResult(res) => {
+                                match res {
+                                    SubscribeResult::Ok => {}
+                                    SubscribeResult::Err(..) => {
+                                        break;
+                                    }
+                                }
+                            }
+                            _ => {}
                         }
                     }
-                    _ => {}
+                    Err(err) => {
+                        println!("{}", err);
+                    }
                 }
+                println!("Réponse du serveur : {:?}", buf);
             }
             Err(err) => {
                 println!("{}", err);
             }
         }
-        println!("Réponse du serveur : {:?}", buf);
+        let response= from_utf8(&response).unwrap();
+        println!("{}", response);
     }
 }
 
